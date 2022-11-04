cce1efad247353156526153ffecd4ad3b8bc7e3c
Mateus <105805491+mateusmarinhoo@users.noreply.github.com>
2022-11-04 20:06:53 -0300

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Mateus Marinho on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
